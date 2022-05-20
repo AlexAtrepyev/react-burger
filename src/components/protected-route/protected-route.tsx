7518430f8308f65440f41fd68f12332f58d5f13e
Alex <atrepyev27@yandex.ru>
2022-05-20 23:06:30 +0300

    refactor: улучшить компонент ProtectedRoute

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,15 +1,15 @@
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
 
 import { TUser } from '../../types';
 
-const ProtectedRoute: FC<{ path: string }> = ({ children, path }) => {
+const ProtectedRoute: FC<RouteProps> = ({ children, ...props }) => {
   const user = useSelector<any, TUser>(state => state.auth.user);
   
   return (
     <Route
-      {...path}
+      {...props}
       render={({ location }) =>
         user.name ? (
           children
